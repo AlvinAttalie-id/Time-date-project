8844bc630b2bebb8e6a3455c12239de35b157c2f
AlvinAttalie-id <Alvin.attalie41@gmail.com>
2024-12-24 15:54:43 +0800

    first Deploy

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submited, setSubmited] = useState(false);
+
+  function handleChange(event) {
+    setSubmited(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmited(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submited ? enteredPlayerName : "Unknown Entity"}</h2>
+      <p>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
