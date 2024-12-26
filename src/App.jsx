21a1ae72ff8e778e581c90d47fab9b353cb4fc3a
AlvinAttalie-id <Alvin.attalie41@gmail.com>
2024-12-26 14:47:56 +0800

    first Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="easy" targetTime={10} />
+        <TimerChallenge title="Medium" targetTime={5} />
+        <TimerChallenge title="Hard" targetTime={3} />
+        <TimerChallenge title="pro" targetTime={1} />
+      </div>
     </>
   );
 }
